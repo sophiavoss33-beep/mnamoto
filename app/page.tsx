93e97673675d9e1dea992e31ee76784f3404c938
MNA Moto <deploy@mnamoto.com.au>
2026-06-29 12:28:27 +0000

    Remove WhyUs, Newsletter and Reviews sections

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/Hero";
 import Categories from "@/components/sections/Categories";
 import FeaturedBikes from "@/components/sections/FeaturedBikes";
-import WhyUs from "@/components/sections/WhyUs";
 import Workshop from "@/components/sections/Workshop";
 import GearSection from "@/components/sections/GearSection";
-import Reviews from "@/components/sections/Reviews";
-import Newsletter from "@/components/sections/Newsletter";
 import BackToTop from "@/components/ui/BackToTop";
 
 export default function Home() {
@@ -18,11 +15,8 @@ export default function Home() {
         <Hero />
         <Categories />
         <FeaturedBikes />
-        <WhyUs />
         <Workshop />
         <GearSection />
-        <Reviews />
-        <Newsletter />
       </main>
       <Footer />
       <BackToTop />
